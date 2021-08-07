1a91801598c3c422d06d43454bdd2b5d1846d631
qhy <+>
2021-08-07 23:57:35 +0800

    成功启动了SpringBoot项目

---
+++ blog-api/src/main/java/com/xiaoqi/blog/config/MyBaticPlusConfig.java
@@ -0,0 +1,20 @@
+package com.xiaoqi.blog.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.xiaoqi.blog.mapper")
+public class MyBaticPlusConfig {
+
+    //分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
